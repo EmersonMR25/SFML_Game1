7c9ab8979498b82fa83ddceab3c9f163cca5fd69
Emerson Romero <eam341@georgetown.edu>
2024-10-10 08:45:16 -0400

    Added a circle that redraws where you click with mouse button

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    window.setFramerateLimit(60);
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    sf::Vector2 position(100.f, 100.f); // Temporarily at the center of the window
 
     while (window.isOpen())
     {
@@ -13,7 +15,15 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    position = sf::Vector2f(static_cast<float>(event.mouseButton.x - 100.f), static_cast<float>(event.mouseButton.y - 100.f));
+                }
+            }
         }
+        shape.setPosition(position); // Move the shape to the current mouse position
 
         window.clear();
         window.draw(shape);
